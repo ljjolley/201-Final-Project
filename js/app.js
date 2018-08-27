45f9af73ca1d6449ab433b4b62562b66dbff1558
Jack Teasell <contact.jackteasell@gmail.com>
2018-08-27 12:42:11 -0700

    updateDate finished has not been checked for bugs yet

--- js/app.js
+++ js/app.js
@@ -12,9 +12,21 @@ var Task = function(taskName, assignedTo, startingDate) {
     assignedTo[randomNumberGenerator(assignedTo.length)];
   allTasks.push(this);
 };
-
+// This checks to see if it is a new day, if it is then it changes who the task is assigned to, if the person assigned yesterday is last in the array it starts again at the beginning of the array
 Task.prototype.updateDate = function() {
-  //
+  if (dayOfWeek !== this.startingDate) {
+    this.startingDate = dayOfWeek;
+    if (
+      this.assignedTo.indexOf(this.currentlyAssignedTo) + 1 >
+      this.assignedTo.length
+    ) {
+      this.currentlyAssignedTo = this.assignedTo[0];
+    } else {
+      this.currentlyAssignedTo = this.assignedTo[
+        this.assignedTo.indexOf(this.currentlyAssignedTo) + 1
+      ];
+    }
+  }
 };
 
 //This function is generates a random number between 0 and the numberOfPeople assigned to the task.
