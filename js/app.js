6bd20281aaa89727ad5695d07fe41a0ffb58cd84
Jack Teasell <contact.jackteasell@gmail.com>
2018-08-27 12:30:03 -0700

    updateDate

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 'use strict';
 var today = new Date();
 var dayOfWeek = today.getDay();
+var allTasks = [];
+
 // This is the task constructor, still needs some work but we do not have much content to work with so far.
-var taskCreator = function(taskName, assignedTo, startingDate) {
+var Task = function(taskName, assignedTo, startingDate) {
   this.taskName = taskName;
   this.assignedTo = assignedTo;
   this.startingDate = startingDate;
   this.currentlyAssignedTo =
     assignedTo[randomNumberGenerator(assignedTo.length)];
+  allTasks.push(this);
 };
 
-taskCreator.prototype.updateDate = function() {
-  //here we need to update the date.
+Task.prototype.updateDate = function() {
+  //
 };
 
 //This function is generates a random number between 0 and the numberOfPeople assigned to the task.
